40efcb0f628f3036079ad106f664161fb79698dd
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-07-27 15:27:10 +0200

    Add - Commentaires

--- db.php
+++ db.php
@@ -1,14 +1,19 @@
 <?php
+/*Nom de la base de donnée*/
 define('DATABASE', 'materiel');
+/*Identifiant de la base de donnée*/ 
 define('USER', 'root');
+/*Mot de passe de la base de donnée*/ 
 define('PWD', '');
+/*Hôte de la base de donnée */
+define('HOST','localhost');
+
 
 /*define('DATABASE', 'adriens_materiel');
 define('USER', 'adriens');
 define('PWD', 'sHXd4ZJ2qCL8rA==');
 */
 
-define('HOST','localhost');
 
 /*Connexion à la base de donnée*/
 try {
