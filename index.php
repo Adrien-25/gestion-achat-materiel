2f1178fc5d84a1fb8d04f7cc782d043cbb43d04f
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-07-29 16:09:32 +0200

    Suppression du formulaire de test

--- index.php
+++ index.php
@@ -29,27 +29,6 @@ $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 //Gestion des formats des dates en français
 $intlDateFormater = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT,IntlDateFormatter::NONE);
 
-foreach($result as $row){
-    echo '<tr>';
-    echo '<td>'.$row['id'].'<br></td>';
-    echo '<td>'.$row['adresse'].'<br></td>';
-    echo '<td>'.$row['url'].'<br></td>';
-    echo '<td>'.$row['nom'].'<br></td>';
-    echo '<td>'.$row['reference'].'<br></td>';
-    echo '<td>'.$row['categorie'].'<br></td>';
-    echo '<td>'.$intlDateFormater->format(strtotime($row['date_achat'])).'<br></td>';
-    echo '<td>'.$intlDateFormater->format(strtotime($row['date_fin_garantie'])).'<br></td>';
-    echo '<td>'.$row['prix'].' €<br></td>';
-    echo '<td>'.$row['conseil_entretien'].'<br></td>';
-    echo '<td>'.$row['ticket_achat'].'<br></td>';
-    echo '<td>'.$row['manuel'].'<br></td>';
-    echo '<td><a href="modifier.php?modifier=1&id='.$row['id'].'">Modifier</a><br></td>';
-    echo '<td><a href="supprimer.php?id='.$row['id'].'">Supprimer</a><br></td>';
-    echo '</tr>';
-    $test = 0;  
-    
-}
-
 $template = $twig->load('pages/index.html.twig');
 echo $template->render();
 ?>
