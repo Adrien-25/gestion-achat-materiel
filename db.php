0f073159b61c64c6c3fb89606202c78e524f8fdc
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-07-29 16:39:53 +0200

    Ajout du port 3308 lors de la connexion à la BDD

--- db.php
+++ db.php
@@ -11,7 +11,7 @@ define('HOST','localhost');
 
 /*Connexion à la base de donnée*/
 try {
-    $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    $dbh = new PDO('mysql:host='.HOST.';port=3308;dbname='.DATABASE, USER, PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
