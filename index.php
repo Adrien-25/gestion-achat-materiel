30b802c88c69e8cda6e72fc23aa546e4d12199cb
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-08-18 10:28:03 +0200

    Ajout de la barre de recherche
    Traitement php de la barre de recherche, si le mot recherché est contenu dans le nom du materiel alors le materiel est affiché

--- index.php
+++ index.php
@@ -17,20 +17,33 @@ if(empty($_SESSION['identifiant'])){
     header('Location: login.php');
 }
 
+if (isset($_POST['search'])){
+    $search = $_POST['search'];
+    //Préparation de la requête
+    $sql= 'SELECT id,adresse, url, nom, reference, categorie, date_achat, date_fin_garantie, prix, conseil_entretien, ticket_achat, manuel FROM materiel WHERE nom LIKE "%'.$search.'%"';
+    $sth = $dbh->prepare($sql);
+    //Exécution de la requête
+    $sth->execute();
+    //On recupère le résultat de la requête
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);  
+    //Gestion des formats des dates en français
+    $intlDateFormater = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT,IntlDateFormatter::NONE);
 
-//Préparation de la requête
-$sql= 'SELECT id,adresse, url, nom, reference, categorie, date_achat, date_fin_garantie, prix, conseil_entretien, ticket_achat, manuel FROM materiel';
-$sth = $dbh->prepare($sql);
-//Exécution de la requête
-$sth->execute();
+} else {
 
-//On recupère le résultat de la requête
-$result = $sth->fetchAll(PDO::FETCH_ASSOC);  
-//Gestion des formats des dates en français
-$intlDateFormater = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT,IntlDateFormatter::NONE);
+    //Préparation de la requête
+    $sql= 'SELECT id,adresse, url, nom, reference, categorie, date_achat, date_fin_garantie, prix, conseil_entretien, ticket_achat, manuel FROM materiel';
+    $sth = $dbh->prepare($sql);
+    //Exécution de la requête
+    $sth->execute();
+    
+    //On recupère le résultat de la requête
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);  
+    //Gestion des formats des dates en français
+    $intlDateFormater = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT,IntlDateFormatter::NONE);
+}
 
 $template = $twig->load('pages/index.html.twig');
-echo $template->render(['liste_produits' => $result,
-'avatar' => $_SESSION['identifiant']]);
+echo $template->render(['liste_produits' => $result,'avatar' => $_SESSION['identifiant'],'email' => $_SESSION['email']]);
 
 ?>
