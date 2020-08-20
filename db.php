33c072ae8ef435ed5ad377a1842e2a67f240e121
fouad-git <fouad.lyousfi@hotmail.com>
2020-08-20 14:48:05 +0200

    mise en place du modal de suppression
    modal ok et changement icones supprimer et modifier

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
+/***En local */
 /*Nom de la base de donnée*/
 define('DATABASE', 'gestion_achat');
 /*Identifiant de la base de donnée*/ 
@@ -9,9 +10,22 @@ define('PWD', '');
 define('HOST','localhost');
 
 
+
+/***En production */
+// /*Nom de la base de donnée*/
+// define('DATABASE', 'fouadl_gestionproduits');
+// /*Identifiant de la base de donnée*/ 
+// define('USER', 'fouadl');
+// /*Mot de passe de la base de donnée*/ 
+// define('PWD', '51n68L9j13Hplw==');
+// /*Hôte de la base de donnée */
+// define('HOST','localhost');
+
+
 /*Connexion à la base de donnée*/
 try {
     $dbh = new PDO('mysql:host='.HOST.';port=3308;dbname='.DATABASE, USER, PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    // $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
