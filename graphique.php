5e371b514273987572cf46739fcd3f8dad136aaf
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-08-21 11:26:40 +0200

    Modification de la requête sql, du teste de depense et de l'envoie de donnée en render

--- graphique.php
+++ graphique.php
@@ -15,12 +15,13 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 if(empty($_SESSION['identifiant'])){
     header('Location: login.php');
 }
-
-$sql = 'SELECT categorie,date_achat,prix FROM materiel ';
-$sth = $dbh->prepare( $sql );
-$sth->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+//Préparation de la requête
+$sql= 'SELECT categorie,date_achat,prix FROM materiel ';
+$sth = $dbh->prepare($sql);
+//Exécution de la requête
 $sth->execute();
-$data = $sth->fetch(PDO::FETCH_ASSOC);
+//On recupère le résultat de la requête
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);  
 
 
 $depense = array(
@@ -39,7 +40,7 @@ $depense = array(
 if (!empty($_POST)){
     $date_debut = $_POST['date_debut'];
     $date_fin = $_POST['date_fin'];
-    foreach($data as $value){
+    foreach($result as $value){
         if (($value['date_achat'] > $date_debut) && ($value['date_achat'] > $date_fin)){
             if(in_array($value['categorie'] , $depense)){
                 $depense[$value['categorie']] += $value['prix'];
@@ -48,8 +49,7 @@ if (!empty($_POST)){
     }
     asort($depense);
 }
-
 $template = $twig->load('pages/graphique.html.twig');
-echo $template->render(['liste_produits' => $data,'liste_produits' => $depense,'avatar' => $_SESSION['identifiant'],'email' => $_SESSION['email']]);
+echo $template->render(['liste_produits' => $result,'depense' => $depense,'avatar' => $_SESSION['identifiant'],'email' => $_SESSION['email']]);
 
 ?>
