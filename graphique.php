cf7c6fa77740fd6b8c5ad8570e4ad0c83bb342ca
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-08-21 16:07:46 +0200

    Tri

--- graphique.php
+++ graphique.php
@@ -35,6 +35,7 @@ $depense = array(
     "Mode" => "0",
     "Autres" => "0",
 );
+
 if (!empty($_POST)){
     $date_debut = $_POST['date_debut'];
     $date_fin = $_POST['date_fin'];
@@ -47,5 +48,5 @@ if (!empty($_POST)){
     //asort($depense);
 }
 $template = $twig->load('pages/graphique.html.twig');
-echo $template->render(['liste_produits' => $result,'depense' => $depense,'avatar' => $_SESSION['identifiant'],'email' => $_SESSION['email']]);
+echo $template->render(['liste_produits' => $result,'date' => $_POST,'depense' => $depense,'avatar' => $_SESSION['identifiant'],'email' => $_SESSION['email']]);
 
