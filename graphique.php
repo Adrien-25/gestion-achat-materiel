0383974beba66a13045a8941cc0dbd19b40ad950
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-08-21 14:36:09 +0200

    Modfication du test pour l'ajout d'un achat et suppression du tri des dépenses

--- graphique.php
+++ graphique.php
@@ -41,13 +41,12 @@ if (!empty($_POST)){
     $date_debut = $_POST['date_debut'];
     $date_fin = $_POST['date_fin'];
     foreach($result as $value){
-        if (($value['date_achat'] > $date_debut) && ($value['date_achat'] > $date_fin)){
-            if(in_array($value['categorie'] , $depense)){
-                $depense[$value['categorie']] += $value['prix'];
-            }
+        if (($value['date_achat'] > $date_debut) && ($value['date_achat'] < $date_fin)){
+            $depense[$value['categorie']] += $value['prix'];
         }
+       
     }
-    asort($depense);
+    //asort($depense);
 }
 $template = $twig->load('pages/graphique.html.twig');
 echo $template->render(['liste_produits' => $result,'depense' => $depense,'avatar' => $_SESSION['identifiant'],'email' => $_SESSION['email']]);
